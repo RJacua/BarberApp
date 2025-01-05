6e20259dcda325cf0f68d1d21ad36c2085c23a07
RJacua <rsjacua@gmail.com>
2025-01-05 18:32:17 +0000

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,79 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.safeargs)
+    id("com.google.devtools.ksp")
+}
+
+android {
+    namespace = "com.example.barberapp"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.barberapp"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    buildFeatures.viewBinding = true
+    // ou: viewBinding.enable = true
+
+}
+
+dependencies {
+    api(libs.cameraview)
+
+    // Image loading (usar uma OU outra)
+    implementation(libs.coil)
+    // implementation(libs.glide)
+
+    implementation(libs.kotlinx.datetime)
+    implementation(libs.androidx.room.runtime)
+    annotationProcessor(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
+
+    // https://github.com/hdodenhof/CircleImageView
+    implementation(libs.circleimageview)
+
+    implementation(libs.androidx.fragment.ktx)
+    implementation(libs.androidx.navigation.fragment)
+    implementation(libs.androidx.navigation.ui)
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.androidx.navigation.runtime.ktx)
+    implementation(libs.androidx.legacy.support.v4)
+    implementation(libs.androidx.recyclerview)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
---
+++ app/src/main/java/com/example/barberapp/HomeClientFragment.kt
@@ -0,0 +1,44 @@
+package com.example.barberapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.barberapp.databinding.FragmentHomeClientBinding
+
+class HomeClientFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeClientBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        binding = FragmentHomeClientBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.btnBarberShop.setOnClickListener {
+            findNavController().navigate(HomeClientFragmentDirections.actionHomeClientFragmentToBarberShopFragment())
+        }
+
+        binding.btnBarber.setOnClickListener {
+            findNavController().navigate(HomeClientFragmentDirections.actionHomeClientFragmentToBarberFragment())
+        }
+
+        binding.btnServices.setOnClickListener {
+            findNavController().navigate(HomeClientFragmentDirections.actionHomeClientFragmentToServiceFragment())
+        }
+
+        binding.btnappointment.setOnClickListener {
+            findNavController().navigate(HomeClientFragmentDirections.actionHomeClientFragmentToAppointmentFragment())
+        }
+
+    }
+
+}
